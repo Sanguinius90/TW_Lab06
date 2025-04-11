a85113dd57906c25006c58e6b5c2b885a61f0cab
Sanguinius90 <kacperkotnisz@gmail.com>
2025-04-11 12:35:04 +0200

    Update config.ts

--- IoT-dashboard/api/lib/config.ts
+++ IoT-dashboard/api/lib/config.ts
@@ -1,4 +1,4 @@
 export const config = {
     port: process.env.PORT || 3100,
-    databaseUrl: process.env.MONGO_URI || 'mongodb+srv://user:asdf@cluster1.cd5eiqt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1'
+    databaseUrl: process.env.MONGO_URI || ''
 };
